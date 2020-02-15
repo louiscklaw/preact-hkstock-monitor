0308ceaa33f0ff387acf8565949057439b52d1fe
louiscklaw <louiscklaw@gmail.com>
2020-02-15 16:00:23 +0800

    update and fix dji header links,

--- src/components/nav/NavCenter.js
+++ src/components/nav/NavCenter.js
@@ -16,6 +16,9 @@ import NavItem from './NavItem';
 
 import lang from '../lang/zh_tw';
 
+// import {DJI_SINGLE_QUOTE_DASHBOARD_ROUTE} from '../../routes/config'
+let DJI_SINGLE_QUOTE_DASHBOARD_ROUTE = 'dji_single_quote_dashboard'
+
 let list_menu_content = [
   { label: lang.LIST_BASIC_LIST, href:'/basic_list' },
   { label: lang.LIST_BUY_SELL_LIST, href:'/buy_sell_list' },
@@ -24,8 +27,15 @@ let list_menu_content = [
 export default class NavRight extends Component{
 
   getMenu(){
+    console.log(MenuList)
     return MenuList.map(x=>{
-      return ( <NavItem text={x.text} href={`/aastock_dashboard/${x.href}`} /> )
+
+      if (x.href.search(DJI_SINGLE_QUOTE_DASHBOARD_ROUTE) >= 0){
+        console.log('href',`/${x.href}`);
+        return ( <NavItem text={x.text} href={`/${x.href}`} /> )
+      }else{
+        return ( <NavItem text={x.text} href={`/aastock_dashboard/${x.href}`} /> )
+      }
     })
   }
 
